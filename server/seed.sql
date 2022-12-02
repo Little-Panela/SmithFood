898d6d99ae7fb2bb8e8e3330300f9e5e723a0086
Fabio Neves <67487494+FabioNeves00@users.noreply.github.com>
2022-12-01 23:28:11 -0300

    Create seed.sql

---
+++ server/seed.sql
@@ -0,0 +1,16 @@
+insert into category values (default, 'Entradas');
+insert into category values (default, 'Principais');
+insert into category values (default, 'Bebidas');
+insert into category values (default, 'Sobremesas');
+
+insert into product values (default, 'Isca de peixe', 'Serve até 2 pessoas', 'https://cooknenjoy.com/wp-content/uploads/2020/02/P1210760.jpg', 12, 5, 35.99, 12.00, 1);
+insert into product values (default, 'Camarão empanado', 'Serve até 2 pessoas', 'https://i0.wp.com/xtudoreceitas.com/wp-content/uploads/Camarao-Empanado-Facil.jpg?w=650&ssl=1', 12, 5, 35.99, 12.00, 1);
+insert into product values (default, 'Coca cola', 'KS ou lata', 'https://images-americanas.b2w.io/produtos/430284306/imagens/coca-cola-ks-ultra-290ml/430285165_1_xlarge.jpg', 12, 5, 6.00, 2.00, 3);
+insert into product values (default, 'Fanta uva', 'KS ou lata', 'https://m.media-amazon.com/images/I/61XLiMgJMZL._AC_SL1200_.jpg', 12, 5, 6.00, 2.00, 3);
+insert into product values (default, 'Guaraná', 'KS ou lata', 'https://riomarrecife.com.br/recife/2020/02/Guaran%C3%A1-Antartica-350ml.jpg', 12, 5, 6.00, 2.00, 3);
+insert into product values (default, 'Filé com fritas', 'Serve até 2 pessoas (Acompanhado de batata frita e arroz branco)', 'https://soubh.uai.com.br/uploads/post/image/8723/main_175929_file-com-fritas.jpg', 12, 5, 55.99, 30.00, 2);
+insert into product values (default, 'Peixe frito', 'Serve até 2 pessoas (Acompanhado arroz branco)', 'https://receitinhas.com.br/wp-content/uploads/2017/07/maxresdefault-1024x576.jpg', 12, 5, 45.99, 30.00, 2);
+insert into product values (default, 'Frango empanado', 'Serve até 2 pessoas (Acompanhado de arroz branco)', 'https://img.itdg.com.br/tdg/images/recipes/000/030/068/328929/328929_original.jpg?mode=crop&width=710&height=400', 12, 5, 45.99, 30.00,2);
+insert into product values (default, 'Picolé', 'Chocalate, brigadeiro, tapioca, e outros', 'https://cdn.irmaospatrocinio.com.br/img/p/1/6/4/3/2/8/164328-large_default.jpg', 12, 5, 10.99, 5.00, 4);
+insert into product values (default, 'Sorvete', 'Baunilha, chocolate ou mista', 'https://receitatodahora.com.br/wp-content/uploads/2017/03/sorvete-de-chocolate-de-liquidificador4.jpg',  12, 5, 10.99, 5.00, 4);
+insert into product values (default, 'Mousse', 'Chocolate ou morango', 'https://www.receitasnestle.com.br/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/d2595778219b25cec5d303ff07a91fde.jpg.webp?itok=hzqHjWP6', 12, 5, 10.99, 5.00, 4);
